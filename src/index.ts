436a2c1976257dd63b88a3e8eb0206b7fa958a47
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-27 14:42:33 +0200

    buildUserCollection created inside User class

--- src/index.ts
+++ src/index.ts
@@ -20,15 +20,20 @@ collection.fetch();
 
 //After Collections class converted to generic class (with T & K and deserialize function)
 
-import { User, UserProps } from './models/User';
-import { Collection } from './models/Collection';
+//import { User, UserProps } from './models/User';
+import { User } from './models/User';
+//import { Collection } from './models/Collection';
 
 //PROBLEM IS: Every time we created a collection, we do not want to send rootUrl, and define the deserialize function!
 //Maybe best place to create collection is User Class!!!
-const collection = new Collection<User, UserProps>(
-  'http://localhost:3000/users',
-  (json: UserProps) => User.buildUser(json)
-);
+// const collection = new Collection<User, UserProps>(
+//   'http://localhost:3000/users',
+//   (json: UserProps) => User.buildUser(json)
+// );
+
+//After adding "buildUSerCollection" method to User class:
+//(We do not need to send any parameters and import UserProps and Collection classes)
+const collection = User.buildUserCollection();
 
 collection.on('allUsersLoaded', () => {
   console.log(collection);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,8 @@
-/*
-import { AxiosResponse } from 'axios';
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-*/
 import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -29,86 +24,9 @@ export class User extends Model<UserProps> {
     );
   }
 
-  // static buildLocalUser(attrs: UserProps): User {
-  //   return new User(
-  //     new Attributes<UserProps>(attrs),
-  //     new Eventing(),
-  //     new LocalSync<UserProps>(rootUrl)
-  //   );
-  // }
-
-  // isAdminUser(): boolean {
-  //   return this.get('id') === 1;
-  // }
-}
-
-//After adding Model class, all things one here moved to Model.ts
-/*
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  //public attributes: Attributes<UserProps> = new Attributes<UserProps>();  //NOT PROPER!
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  //IF NOT defined as getter/accessor function, then its strongly dependent on the how "on" defined in Eventing class.
-  //If something changes in the definition on  the "on" function, all classes like this using on from Eventing, must be changed.
-  //Pain in the ass.
-
-  // on(eventName: string, callback: Callback): void {
-  //   this.events.on(eventName, callback);
-  // }
-
-  //Here is the solution:
-  //Anytime someone references this on property I will return "this.events.on".
-  //Now notice how I'm NOT calling "on" here. So the real leap of faith you need to make here
-  //the real important thing to understand is that I am NOT trying to call a function right here.
-  //Instead I'm trying to return a reference to the "events.on" method.
-
-  //ALL THE FUNCTIONS BELOW MOVED TO Model CLASS, TO IMPLEMENT "INHERITANCE" STYLE APPROACH
-    get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    //const id = this.attributes.get('id');
-    const id = this.get('id'); //nothing much difference with the line commented
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      //this.attributes.set(response.data);
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
+    );
   }
 }
-*/
