cd3565e5f9753d37d262f5145226f7091496c39f
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-27 16:32:15 +0200

    showing user data on the UserForm / dom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 //with this example we can show some basic content on the dom
 //before the changes on this file, the div named/id-ed "root" added to the "index.html"
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+//const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'ali', age: 20 });
+const userForm = new UserForm(document.getElementById('root'), user);
 userForm.render();
