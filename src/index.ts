e0ad366f60ab187b37aa234a7af08a22614f8e96
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-27 14:09:56 +0200

    Collection class converted to generic class & its tested from index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,29 @@ axios.get('http://localhost:3000/users').then((response: AxiosResponse) => {
 });
 */
 
+/*
 import { Collection } from './models/Collection';
 
 const collection = new Collection('http://localhost:3000/users');
+
+collection.on('allUsersLoaded', () => {
+  console.log(collection);
+});
+
+collection.fetch();
+*/
+
+//After Collections class converted to generic class (with T & K and deserialize function)
+
+import { User, UserProps } from './models/User';
+import { Collection } from './models/Collection';
+
+//PROBLEM IS: Every time we created a collection, we do not want to send rootUrl, and define the deserialize function!
+//Maybe best place to create collection is User Class!!!
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on('allUsersLoaded', () => {
   console.log(collection);
--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,14 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
-
-import { User, UserProps } from './User';
+//import { User, UserProps } from './User';
 import { Eventing } from './Eventing';
 
 //CURRENT PROBLEM ABOUT THIS CLASS IS, ITS NOT GENERIC, ONLY CAN WORK WITH USER CLASS
-export class Collection {
-  models: User[] = [];
+//export class Collection {
+//T: might be User and K might be UserProps
+export class Collection<T, K> {
+  //models: User[] = [];
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   //why we are not using shortened version of getters/accessors?
   //because properties of this class defined inline style, not as constructor params
@@ -26,9 +28,11 @@ export class Collection {
       // axios.get('http://localhost:3000/users').then((response: AxiosResponse) => {
       //     console.log(response.data);
       // });
-      response.data.forEach((value: UserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      //response.data.forEach((value: UserProps) => {
+      response.data.forEach((value: K) => {
+        //const user = User.buildUser(value);
+        //this.models.push(user);
+        this.models.push(this.deserialize(value));
       });
 
       this.trigger('allUsersLoaded');
