6c6355fa6ef885f0a67f1e24f3d72ca97079bb6f
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-27 13:53:20 +0200

    Collections class done - but its not generic enough

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,17 @@
-import { User } from './models/User';
-
-const user = User.buildUser({ id: 1 });
-user.on('change', () => {
-  console.log(user);
-});
-user.fetch(); //fetch defined in Model, and it calls sync.fetch actually
-
 /*
-const user = new User({ name: 'Ghengiz', age: 45 });
+import axios, { AxiosResponse } from 'axios';
 
-console.log(user.get('name'));
-//"get" method (in Attributes class) /property (in User class) refers to "user" in Attributes class.
-//But it must refer to "Attributes" class itself. "data" property defined in Attributes, not in User.
-//The solution for this problem is, defining "get" method as "arrow function" in the Attributes class.
-
-const on = user.on;
-
-//the "on" call below does not call /execute callback, just assigns the callback to "change" keyword
-on('change', () => {
-  console.log('user changed!');
+axios.get('http://localhost:3000/users').then((response: AxiosResponse) => {
+  console.log(response.data);
 });
-
-// OR, directly call is also possible!
-
-//the "on" call below does not call /execute callback, just assigns the callback to "save" keyword
-user.on('save', () => {
-  console.log('user saved!');
-});
-
-//here is the calls/executions!
-user.trigger('change');
-user.trigger('save');
-
-//"set" function test
-user.set({ name: 'mamud' });
-
-//"fetch" function test
-const user2 = new User({ id: 3 });
-
-user2.on('change', () => {
-  console.log(user2);
-});
-
-user2.fetch();
-
-//testing user.save:
-
-const user3 = new User({ id: 5, name: 'newer name', age: 0 });
-
-user3.on('save', () => {
-  console.log(user3);
-});
-
-user3.save();
-
 */
 
-/*
-const colors = {
-  color: 'red';
-  printColor() {
-    console.log(this.color)
-  }
-}
+import { Collection } from './models/Collection';
 
-colors.printColor(); //NO PROBLEM, "this" REFERS TO "colors"
+const collection = new Collection('http://localhost:3000/users');
 
-//HERE IS THE PROBLEM:
-//reference to "colors" lost
-const printCol = colors.printColor;
-printCol();  //try to execute "undefined.printColor()"
-*/
+collection.on('allUsersLoaded', () => {
+  console.log(collection);
+});
+
+collection.fetch();
--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,9 +1,14 @@
-import { User } from './User';
+import axios, { AxiosResponse } from 'axios';
+
+import { User, UserProps } from './User';
 import { Eventing } from './Eventing';
 
+//CURRENT PROBLEM ABOUT THIS CLASS IS, ITS NOT GENERIC, ONLY CAN WORK WITH USER CLASS
 export class Collection {
   models: User[] = [];
   events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string) {}
 
   //why we are not using shortened version of getters/accessors?
   //because properties of this class defined inline style, not as constructor params
@@ -13,5 +18,20 @@ export class Collection {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      //response.data is array of users, tested on the index.ts by the code like below:
+      // axios.get('http://localhost:3000/users').then((response: AxiosResponse) => {
+      //     console.log(response.data);
+      // });
+      response.data.forEach((value: UserProps) => {
+        const user = User.buildUser(value);
+        this.models.push(user);
+      });
+
+      this.trigger('allUsersLoaded');
+    });
   }
 }
